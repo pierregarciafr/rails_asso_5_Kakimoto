023a0e67cb5e33b84ee4f914a741e5093d9a7025
Pierre Garcia <pg.pierre.garcia@free.fr>
2020-07-19 19:56:35 +0200

    implement fixtures and model tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  belongs_to :asso, :optional => true
+  belongs_to :asso, optional: true, dependent: :destroy
   accepts_nested_attributes_for :asso # it works
 
+  def self.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
--- test/models/asso_test.rb
+++ test/models/asso_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class AssoTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  self.use_instantiated_fixtures = true   # for l 24 to work
+
+  def setup
+    @asso = Asso.new(name: 'asso_name')
+  end
+
+  test "asso shouldn't validate without name" do
+    @asso.name = ''
+    assert_not @asso.valid?
+  end
+
+  test "asso should validate if name" do
+    assert_not @asso.name.empty?
+    assert @asso.valid?
+  end
+
+
 end
