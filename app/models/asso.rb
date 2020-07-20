e1b412e513954a659a8ed28238bb4c05173f3e04
Pierre Garcia <pg.pierre.garcia@free.fr>
2020-07-20 20:39:47 +0200

    check models

--- app/models/asso.rb
+++ app/models/asso.rb
@@ -2,5 +2,5 @@ class Asso < ApplicationRecord
   # has_one :user #, :class_name => 'User'
   has_one :user, as: :loginable, dependent: :destroy
 
-  validates :name, presence: true
+  # validates :name, presence: true
 end
