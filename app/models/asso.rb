85be916f1fbeeef462095b5886e14062bf779248
Pierre Garcia <pg.pierre.garcia@free.fr>
2020-07-20 19:02:44 +0200

    change models for polymorphic

--- app/models/asso.rb
+++ app/models/asso.rb
@@ -1,4 +1,6 @@
 class Asso < ApplicationRecord
-  has_one :user #, :class_name => 'User'
+  # has_one :user #, :class_name => 'User'
+  has_one :user, as: :loginable, dependent: :destroy
+
   validates :name, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  belongs_to :asso, optional: true, dependent: :destroy
-  accepts_nested_attributes_for :asso # it works
+
+  # belongs_to :asso, optional: true, dependent: :destroy
+  # accepts_nested_attributes_for :asso
+
+  belongs_to :loginable, polymorphic: true , optional: true
+  accepts_nested_attributes_for :loginable
 
   def self.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
