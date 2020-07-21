2a444f9c0c08866540615645e771de91fdd4fe36
Pierre Garcia <pg.pierre.garcia@free.fr>
2020-07-21 13:58:56 +0200

    adapt register new format in fixtures and tests

--- test/models/asso_test.rb
+++ test/models/asso_test.rb
@@ -17,11 +17,16 @@ class AssoTest < ActiveSupport::TestCase
     assert_not @asso.valid?
   end
 
+  test "register should be unique" do
+    @asso.register = @ringos.register
+    assert_not @asso.valid?
+  end
+
   test "valid format register should be accepted" do
-    registers = ['W123456789', 'y987645433']
+    registers = ['W123456780', 'y987645433']
     registers.each do |valid_register|
       @asso.register = valid_register
-      assert @asso.valid?
+      assert @asso.valid?, "#{valid_register} not accepted"
     end
   end
 
@@ -29,9 +34,7 @@ class AssoTest < ActiveSupport::TestCase
     registers = ['W1234', 'ezrys98765', 'azertyuio']
     registers.each do |invalid_register|
       @asso.register = invalid_register
-      assert_not @asso.valid?
+      assert_not @asso.valid?, "#{invalid_register} not accepted"
     end
   end
-
-
 end
