be52a094bd021f689abc59fd3026d960ce1b518d
Pierre Garcia <pg.pierre.garcia@free.fr>
2020-07-21 13:29:22 +0200

    add register validation to Asso

--- app/models/asso.rb
+++ app/models/asso.rb
@@ -2,5 +2,6 @@ class Asso < ApplicationRecord
   # has_one :user #, :class_name => 'User'
   has_one :user, as: :loginable, dependent: :destroy
 
-  # validates :name, presence: true
+  validates :name, presence: true
+  validates :register, presence: true, format: { with: /\A[a-zA-Z]\d{8}\z/ }
 end
--- test/models/asso_test.rb
+++ test/models/asso_test.rb
@@ -7,14 +7,30 @@ class AssoTest < ActiveSupport::TestCase
     @asso = Asso.new(name: 'asso_name')
   end
 
-  test "asso shouldn't validate without name" do
-    @asso.name = ''
+  test "name should be present" do
+    @asso.name = '   '
     assert_not @asso.valid?
   end
 
-  test "asso should validate if name" do
-    assert_not @asso.name.empty?
-    assert @asso.valid?
+  test "register should be present" do
+    @asso.register = '    '
+    assert_not @asso.valid?
+  end
+
+  test "valid format register should be accepted" do
+    registers = ['W12345678', 'y98765433']
+    registers.each do |valid_register|
+      @asso.register = valid_register
+      assert @asso.valid?
+    end
+  end
+
+  test "bad format register should be recused" do
+    registers = ['W1234', 'ezry98765', 'azertyuio']
+    registers.each do |invalid_register|
+      @asso.register = invalid_register
+      assert_not @asso.valid?
+    end
   end
 
 
